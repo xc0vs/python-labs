5925cbe9546941e9080f84e88e10623901b36c19
xcovs <xcovs@proton.me>
2025-10-27 23:26:31 +0200

    completed task 1

--- lab2/task1.py
+++ lab2/task1.py
@@ -1,15 +1,16 @@
 log_file_path = "./apache_logs.txt"
 
 def analyze_log_file(log_file_path):
-    status_codes = {" 200 " : 0, " 301 " : 0, " 302 " : 0, " 304 " : 0, " 404 " : 0, " 500 " : 0, " 502 " : 0, " 503 " : 0, " 504 " : 0}
+    status_codes = {}
     try:
         # Треба переробити повністю логіку
 
         with open(log_file_path, "rt") as f: 
             for line in f:
-                for code in status_codes:
-                    if code in line:
-                        status_codes[code] += 1
+                parts = line.split('"')
+                code = parts[2].strip().split(' ')[0]
+                if code.isdigit():
+                    status_codes[code] = status_codes.get(code, 0) + 1
         return status_codes
     except FileNotFoundError:
         return "File Not Found Error"
