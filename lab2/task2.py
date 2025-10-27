3d45d213f531199587b0bf56968a0d46e8c37e2b
xcovs <xcovs@proton.me>
2025-10-28 00:37:08 +0200

    several fixes have been added to task 2

--- lab2/task2.py
+++ lab2/task2.py
@@ -1,19 +1,22 @@
 from hashlib import sha256 
 
+file_paths = ["./task2_files/lorem", "./task2_files/steam.exe", "./task2_files/video.dll"]
+
 def generate_file_hashes(*file_paths):
     dic_hashes = {}
     for path in file_paths:
         try:
-            with open(path, "+br") as f:
+            with open(path, "rb") as f:
                 file_data = f.read()
                 dic_hashes[path] = sha256(file_data).hexdigest()
         except FileNotFoundError:
-            return "File Not Found Error"
+            print(f"File Not Found Error: '{path}'")
         except IOError:
-            return "IO Error"
+            print(f"IO Error: '{path}'")
         except:
-            return "An undefined error has occurred"
+            print(f"An undefined error has occurred: '{path}'")
     return dic_hashes
 
-file_paths = ["./task2_files/lorem", "./task2_files/steam.exe", "./task2_files/video.dll"]
-print(generate_file_hashes(*file_paths))
+
+hashes = generate_file_hashes(*file_paths)
+print(hashes)
